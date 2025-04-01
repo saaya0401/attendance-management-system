c3773097ab7954b9b1e2cbe2b892d365834b5d95
koba saaya <saaya.koba0401@gmail.com>
2025-04-02 06:47:40 +0900

    fortify設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\StaffController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/admin/login', [AdminController::class, 'adminLoginView'])->name('admin.login');
+Route::post('/admin/login', [AdminController::class, 'adminLogin'])->name('admin.login');
+Route::post('/register' , [StaffController::class, 'register'])->name('register');
+Route::post('/login', [StaffController::class, 'login'])->name('login');
+
