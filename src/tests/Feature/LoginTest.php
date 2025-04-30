b238554e860e102dac808bc30cda2797b8e6947b
koba saaya <saaya.koba0401@gmail.com>
2025-04-30 14:50:08 +0900

    logintest記載

---
+++ src/tests/Feature/LoginTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testEmailEmpty(){
+        User::create([
+            'name'=>'test',
+            'email'=>'test@example.com',
+            'password'=>bcrypt('password123')
+        ]);
+
+        $response=$this->get('/login');
+        $response->assertStatus(200);
+
+        $response=$this->post('/login', [
+            'email'=>'',
+            'password'=>'password123'
+        ]);
+        $response->assertSessionHasErrors(['email'=>'メールアドレスを入力してください']);
+    }
+
+    public function testPasswordEmpty(){
+        User::create([
+            'name'=>'test',
+            'email'=>'test@example.com',
+            'password'=>bcrypt('password123')
+        ]);
+
+        $response=$this->get('/login');
+        $response->assertStatus(200);
+
+        $response=$this->post('/login', [
+            'email'=>'test@example.com',
+            'password'=>''
+        ]);
+        $response->assertSessionHasErrors(['password'=>'パスワードを入力してください']);
+    }
+
+    public function testLoginDifferent(){
+        User::create([
+            'name'=>'test',
+            'email'=>'test@example.com',
+            'password'=>bcrypt('password123')
+        ]);
+
+        $response=$this->get('/login');
+        $response->assertStatus(200);
+
+        $response=$this->post('/login', [
+            'email'=>'testtest@example.com',
+            'password'=>'password123'
+        ]);
+        $response->assertSessionHasErrors(['email'=>'ログイン情報が登録されていません']);
+    }
+}
